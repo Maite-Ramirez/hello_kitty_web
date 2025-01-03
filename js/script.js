745a0991fe55df7ac6a7fac371f9907184f85178
mdltm <imaginamartina@gmail.com>
2025-01-03 11:20:06 +0100

    libraries set up

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let $document = $(document)
+$document.ready(function(){
+
+    AOS.init();
+
+//$("#projects-guide > div > a").on("click", function(){
+        //console.log($("#projects-guide-open").css("display"));
+      
+        //let actual_chevron = $("#projects-guide-open").css("display");
+      
+        //if (actual_chevron === "none") {
+           //$("#projects-guide-open").css("display", "flex");
+                  
+        //} else {
+           //$("#projects-guide-open").css("display", "none");
+        //}
+      
+    //});
+
+
+
+
+
+
+
+});
